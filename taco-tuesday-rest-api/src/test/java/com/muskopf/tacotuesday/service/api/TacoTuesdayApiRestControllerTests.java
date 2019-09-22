1d8f5e73e63ac840fdc1fdaf560c72245bacf82c
Justin Muskopf <jaymuskopf@gmail.com>
2019-09-22 17:36:55 -0500

    Comment out test classes for the time being

--- taco-tuesday-rest-api/src/test/java/com/muskopf/tacotuesday/service/api/TacoTuesdayApiRestControllerTests.java
+++ taco-tuesday-rest-api/src/test/java/com/muskopf/tacotuesday/service/api/TacoTuesdayApiRestControllerTests.java
@@ -1,11 +1,17 @@
 package com.muskopf.tacotuesday.service.api;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/*
 @RunWith(SpringRunner.class)
 @WebMvcTest(TacoTuesdayApiRestController.class)
 public class TacoTuesdayApiRestControllerTests {
-
+    @Test
+    public void placeholder() {
+        assert(true);
+    }
 }
+*/
