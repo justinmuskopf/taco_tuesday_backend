27a81e317408ac49c1f6f0f53f65b86e2feae205
Justin Muskopf <jaymuskopf@gmail.com>
2019-09-22 13:42:08 -0500

    Pre-generate API Key when Employee is persisted, create full name field

---
+++ taco-tuesday-rest-api/src/main/java/com/respec/tacotuesday/bl/impl/TacoTuesdayDAOImpl.java
@@ -0,0 +1,134 @@
+package com.respec.tacotuesday.bl.impl;
+
+import com.respec.tacotuesday.bl.TacoTuesdayDAO;
+import com.respec.tacotuesday.bl.repository.EmployeeRepository;
+import com.respec.tacotuesday.bl.repository.FullOrderRepository;
+import com.respec.tacotuesday.bl.repository.IndividualOrderRepository;
+import com.respec.tacotuesday.domain.Employee;
+import com.respec.tacotuesday.domain.FullOrder;
+import com.respec.tacotuesday.domain.IndividualOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityExistsException;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.isNull;
+
+@Component
+@Transactional
+public class TacoTuesdayDAOImpl implements TacoTuesdayDAO {
+    private EmployeeRepository employeeRepository;
+    private FullOrderRepository fullOrderRepository;
+    private IndividualOrderRepository individualOrderRepository;
+
+    @Autowired
+    public TacoTuesdayDAOImpl(EmployeeRepository employeeRepository,
+                              FullOrderRepository fullOrderRepository,
+                              IndividualOrderRepository individualOrderRepository)
+    {
+        this.employeeRepository = employeeRepository;
+        this.fullOrderRepository = fullOrderRepository;
+        this.individualOrderRepository = individualOrderRepository;
+    }
+
+    private FullOrder getFullOrderIfPresent(Integer id) {
+        Optional<FullOrder> optionallyExistingOrder = fullOrderRepository.findById(id);
+        return optionallyExistingOrder.orElse(null);
+    }
+
+    private IndividualOrder getIndividualOrderIfPresent(Integer id) {
+        Optional<IndividualOrder> optionallyExistingOrder = individualOrderRepository.findById(id);
+        return optionallyExistingOrder.orElse(null);
+    }
+
+    @Override
+    public IndividualOrder updateIndividualOrder(IndividualOrder order) {
+        IndividualOrder existingOrder = getIndividualOrderIfPresent(order.getId());
+        if (existingOrder == null) {
+            // TODO Return a BAD REQUEST?
+            return null;
+        }
+
+        existingOrder.merge(order);
+
+        individualOrderRepository.save(existingOrder);
+
+        return existingOrder;
+    }
+
+    @Override
+    public FullOrder updateFullOrder(FullOrder order) {
+        FullOrder existingOrder = getFullOrderIfPresent(order.getId());
+        if (existingOrder == null) {
+            // TODO Return a BAD REQUEST?
+            return null;
+        }
+
+        existingOrder.merge(order);
+
+        fullOrderRepository.save(existingOrder);
+
+        return order;
+    }
+
+    @Override
+    public IndividualOrder retrieveIndividualOrder(Integer id) {
+        return getIndividualOrderIfPresent(id);
+    }
+
+    @Override
+    public List<IndividualOrder> retrieveAllIndividualOrders() {
+        return individualOrderRepository.findAll();
+    }
+
+    @Override
+    public List<IndividualOrder> retrieveAllIndividualOrdersContainingEmployeeId(Integer employeeId) {
+        return individualOrderRepository.findByEmployeeId(employeeId);
+    }
+
+    @Override
+    public FullOrder retrieveFullOrder(Integer id) {
+        return getFullOrderIfPresent(id);
+    }
+
+    @Override
+    public FullOrder createFullOrder(FullOrder order) {
+        return fullOrderRepository.save(order);
+    }
+
+    @Override
+    public List<FullOrder> retrieveAllFullOrders() {
+        return fullOrderRepository.findAll();
+    }
+
+    @Override
+    public List<FullOrder> retrieveAllFullOrdersContainingEmployeeId(Integer employeeId) {
+        return retrieveAllIndividualOrdersContainingEmployeeId(employeeId)
+                .stream()
+                .map(IndividualOrder::getFullOrder)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Employee createEmployee(String firstName, String lastName, String nickName) {
+        if (isNull(firstName) || isNull(lastName)) {
+            throw new IllegalArgumentException("Both first name and last name are required when creating an employee!");
+        }
+
+        boolean employeeAlreadyExists = employeeRepository.existsEmployeeByFirstNameAndLastName(firstName, lastName);
+        if (employeeAlreadyExists) {
+            throw new EntityExistsException("The requested employee already exists!");
+        }
+
+        Employee employee = new Employee()
+                .firstName(firstName)
+                .lastName(lastName)
+                .nickName(nickName);
+
+        return employeeRepository.save(employee);
+    }
+}
--- taco-tuesday-rest-api/src/main/java/com/respec/tacotuesday/domain/Employee.java
+++ taco-tuesday-rest-api/src/main/java/com/respec/tacotuesday/domain/Employee.java
@@ -1,9 +1,12 @@
 package com.respec.tacotuesday.domain;
 
+import com.respec.tacotuesday.bl.proc.ApiKeyGenerator;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 @Entity
 @Table(name = "employee")
@@ -18,9 +21,21 @@ public class Employee {
     private String lastName;
     @Column
     private String nickName;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "api_key_id", referencedColumnName = "id")
+    private ApiKey apiKey;
 
     @OneToMany(mappedBy = "employee", cascade = CascadeType.PERSIST)
     private List<IndividualOrder> orders;
+
+    @PrePersist
+    public void ensureApiKeyIsPopulated() {
+        if (!isNull(this.apiKey)) {
+            return;
+        }
+
+        this.apiKey = ApiKeyGenerator.generateForEmployee(this);
+    }
 
     public String getFirstName() {
         return firstName;
@@ -44,6 +59,14 @@ public class Employee {
         return this;
     }
 
+    public String getFullName() {
+        if (isNull(firstName) || isNull(lastName)) {
+            return null;
+        }
+
+        return firstName + " " + lastName;
+    }
+
     public String getNickName() { return nickName; }
     public void setNickName(String nickName) { this.nickName = nickName; }
     public Employee nickName(String nickName) {
@@ -58,12 +81,8 @@ public class Employee {
         this.id = id;
     }
 
-    public List<IndividualOrder> getOrders() {
-        return orders;
-    }
-    public void setOrders(List<IndividualOrder> orders) {
-        this.orders = orders;
-    }
+    public List<IndividualOrder> getOrders() { return orders; }
+    public void setOrders(List<IndividualOrder> orders) { this.orders = orders; }
     public Employee orders(List<IndividualOrder> orders) {
         this.orders = orders;
         return this;
