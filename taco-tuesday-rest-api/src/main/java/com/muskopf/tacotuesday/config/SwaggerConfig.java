85dffddb161d0b381ebc34a2c0ea787a10a14c2e
Justin Muskopf <Justin.Muskopf@RESPEC.com>
2019-09-23 12:33:04 -0500

    Add Swagger UI

--- taco-tuesday-rest-api/src/main/java/com/muskopf/tacotuesday/config/SwaggerConfig.java
+++ taco-tuesday-rest-api/src/main/java/com/muskopf/tacotuesday/config/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package com.muskopf.tacotuesday.config;
 
 import com.google.common.base.Predicate;
+import com.muskopf.tacotuesday.service.api.TacoTuesdayApiRestController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -22,6 +24,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                .apis(RequestHandlerSelectors.basePackage("com.muskopf.tacotuesday.service.api"))
                 .paths(PathSelectors.any())
                 .build();
     }
