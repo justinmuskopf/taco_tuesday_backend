b1c9ed9027e484e78ba33f16fbeff6d4e5f30179
Justin Muskopf <jaymuskopf@gmail.com>
2019-09-24 17:38:42 -0500

    Delete TacoTypes  and update server port

--- taco-tuesday-rest-api/src/main/java/com/muskopf/tacotuesday/domain/Taco.java
+++ taco-tuesday-rest-api/src/main/java/com/muskopf/tacotuesday/domain/Taco.java
@@ -2,25 +2,18 @@ package com.muskopf.tacotuesday.domain;
 
 import com.fasterxml.jackson.annotation.*;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "type")
-@JsonSubTypes(value = {
-        @JsonSubTypes.Type(value = BarbacoaTaco.class, name = "BARBACOA"),
-        @JsonSubTypes.Type(value = BeefFajitaTaco.class, name = "BEEF_FAJITA"),
-        @JsonSubTypes.Type(value = CabezaTaco.class, name = "CABEZA"),
-        @JsonSubTypes.Type(value = CarnitasTaco.class, name = "CARNITAS"),
-        @JsonSubTypes.Type(value = ChickenFajitaTaco.class, name = "CHICKEN_FAJITA"),
-        @JsonSubTypes.Type(value = LenguaTaco.class, name = "LENGUA"),
-        @JsonSubTypes.Type(value = PastorTaco.class, name = "PASTOR"),
-        @JsonSubTypes.Type(value = TripaTaco.class, name = "TRIPA"),
-})
-public abstract class Taco {
-    protected Float price;
+public class Taco {
+    private Float price;
+    private TacoType type;
 
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
+    public Taco(TacoType type, Float price) {
+        this.type = type;
         this.price = price;
     }
+
+    public Float getPrice() {return price;}
+    public void setPrice(Float price) {this.price = price;}
+
+    public TacoType getType() {return type;}
+    public void setType(TacoType type) {this.type = type;}
 }
--- taco-tuesday-rest-api/src/main/java/com/muskopf/tacotuesday/domain/TacoPriceList.java
+++ taco-tuesday-rest-api/src/main/java/com/muskopf/tacotuesday/domain/TacoPriceList.java
@@ -1,5 +1,6 @@
 package com.muskopf.tacotuesday.domain;
 
+import com.muskopf.tacotuesday.config.TacoTuesdayApiConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,110 +9,22 @@ import java.util.List;
 
 @Component
 public class TacoPriceList {
-    private BarbacoaTaco barbacoaTaco;
-    private BeefFajitaTaco beefFajitaTaco;
-    private CabezaTaco cabezaTaco;
-    private CarnitasTaco carnitasTaco;
-    private ChickenFajitaTaco chickenFajitaTaco;
-    private LenguaTaco lenguaTaco;
-    private PastorTaco pastorTaco;
-    private TripaTaco tripaTaco;
+    private List<Taco> tacos = new ArrayList<>();
 
     @Autowired
-    public TacoPriceList(BarbacoaTaco barbacoaTaco,
-                         BeefFajitaTaco beefFajitaTaco,
-                         CabezaTaco cabezaTaco,
-                         CarnitasTaco carnitasTaco,
-                         ChickenFajitaTaco chickenFajitaTaco,
-                         LenguaTaco lenguaTaco,
-                         PastorTaco pastorTaco,
-                         TripaTaco tripaTaco)
+    public TacoPriceList(TacoTuesdayApiConfiguration config)
     {
-        this.barbacoaTaco = barbacoaTaco;
-        this.beefFajitaTaco = beefFajitaTaco;
-        this.cabezaTaco = cabezaTaco;
-        this.carnitasTaco = carnitasTaco;
-        this.chickenFajitaTaco = chickenFajitaTaco;
-        this.lenguaTaco = lenguaTaco;
-        this.pastorTaco = pastorTaco;
-        this.tripaTaco = tripaTaco;
-    }
-
-    public BarbacoaTaco getBarbacoaTaco() {
-        return barbacoaTaco;
-    }
-
-    public void setBarbacoaTaco(BarbacoaTaco barbacoaTaco) {
-        this.barbacoaTaco = barbacoaTaco;
-    }
-
-    public BeefFajitaTaco getBeefFajitaTaco() {
-        return beefFajitaTaco;
-    }
-
-    public void setBeefFajitaTaco(BeefFajitaTaco beefFajitaTaco) {
-        this.beefFajitaTaco = beefFajitaTaco;
-    }
-
-    public CabezaTaco getCabezaTaco() {
-        return cabezaTaco;
-    }
-
-    public void setCabezaTaco(CabezaTaco cabezaTaco) {
-        this.cabezaTaco = cabezaTaco;
-    }
-
-    public CarnitasTaco getCarnitasTaco() {
-        return carnitasTaco;
-    }
-
-    public void setCarnitasTaco(CarnitasTaco carnitasTaco) {
-        this.carnitasTaco = carnitasTaco;
-    }
-
-    public ChickenFajitaTaco getChickenFajitaTaco() {
-        return chickenFajitaTaco;
-    }
-
-    public void setChickenFajitaTaco(ChickenFajitaTaco chickenFajitaTaco) {
-        this.chickenFajitaTaco = chickenFajitaTaco;
-    }
-
-    public LenguaTaco getLenguaTaco() {
-        return lenguaTaco;
-    }
-
-    public void setLenguaTaco(LenguaTaco lenguaTaco) {
-        this.lenguaTaco = lenguaTaco;
-    }
-
-    public PastorTaco getPastorTaco() {
-        return pastorTaco;
-    }
-
-    public void setPastorTaco(PastorTaco pastorTaco) {
-        this.pastorTaco = pastorTaco;
-    }
-
-    public TripaTaco getTripaTaco() {
-        return tripaTaco;
-    }
-
-    public void setTripaTaco(TripaTaco tripaTaco) {
-        this.tripaTaco = tripaTaco;
+        tacos.add(new Taco(TacoType.BARBACOA, config.getBarbacoaPrice()));
+        tacos.add(new Taco(TacoType.BEEF_FAJITA, config.getBeefFajitaPrice()));
+        tacos.add(new Taco(TacoType.CABEZA, config.getCabezaPrice()));
+        tacos.add(new Taco(TacoType.CARNITAS, config.getCarnitasPrice()));
+        tacos.add(new Taco(TacoType.CHICKEN_FAJITA, config.getChickenFajitaPrice()));
+        tacos.add(new Taco(TacoType.LENGUA, config.getLenguaPrice()));
+        tacos.add(new Taco(TacoType.PASTOR, config.getPastorPrice()));
+        tacos.add(new Taco(TacoType.TRIPA, config.getTripaPrice()));
     }
 
     public List<Taco> getPriceList() {
-        List<Taco> tacos =  new ArrayList<>();
-        tacos.add(barbacoaTaco);
-        tacos.add(beefFajitaTaco);
-        tacos.add(cabezaTaco);
-        tacos.add(carnitasTaco);
-        tacos.add(chickenFajitaTaco);
-        tacos.add(lenguaTaco);
-        tacos.add(pastorTaco);
-        tacos.add(tripaTaco);
-
         return tacos;
     }
 }
