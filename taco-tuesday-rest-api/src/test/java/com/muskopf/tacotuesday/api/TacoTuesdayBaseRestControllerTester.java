9e9fd2249da07a5fbdf33591c6f6eefa12029d03
Justin Muskopf <jaymuskopf@gmail.com>
2019-11-29 20:03:55 -0600

    Add base controller test class

--- taco-tuesday-rest-api/src/test/java/com/muskopf/tacotuesday/api/TacoTuesdayBaseRestControllerTester.java
+++ taco-tuesday-rest-api/src/test/java/com/muskopf/tacotuesday/api/TacoTuesdayBaseRestControllerTester.java
@@ -1,4 +1,45 @@
 package com.muskopf.tacotuesday.api;
 
+import com.muskopf.tacotuesday.TacoTuesdayApiHelper;
+import com.muskopf.tacotuesday.TacoTuesdayPersistenceInitializer;
+import com.muskopf.tacotuesday.bl.proc.TacoEmailer;
+import com.muskopf.tacotuesday.bl.proc.TacoTuesdayResourceMapper;
+import com.muskopf.tacotuesday.config.TacoTuesdayApiConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = {TacoTuesdayApiConfiguration.class})
+@ComponentScan(basePackages = "com.muskopf.tacotuesday")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class TacoTuesdayBaseRestControllerTester {
+    @MockBean
+    protected TacoEmailer tacoEmailer;
+
+    @Autowired
+    protected TacoTuesdayPersistenceInitializer persistenceHelper;
+    @Autowired
+    protected TacoTuesdayApiHelper apiHelper;
+
+    @Autowired
+    protected TacoTuesdayResourceMapper mapper;
+
+    @Before
+    public void setup() {
+        persistenceHelper.initializeDatabase();
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
 }
